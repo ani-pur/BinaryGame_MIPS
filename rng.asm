91da0657986db7ed64266a8ab647bec059cdcd1a
ani-pur <anirudhpurohit1101@gmail.com>
2025-10-30 23:39:16 -0500

    Update rng.asm

--- rng.asm
+++ rng.asm
@@ -6,7 +6,7 @@
 # Return: $v0, value between [0,$a1] - can change to adjust level bias
 rand_10bit:
     li   $v0, 42          # random int
-    li   $a1, 512        # upper bound (exclusive)
+    li   $a1, 1024        # upper bound (exclusive)
     syscall               # result in $a0
     move $v0, $a0         # return in v0
     jr   $ra
